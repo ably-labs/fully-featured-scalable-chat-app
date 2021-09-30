a0ac47abda73a8ac52f3d2324476c39b65e7e943
thisisjofrank <jofranchetti@gmail.com>
2021-09-30 11:11:47 +0100

    adding some basic react starter pages

---
+++ app/src/script.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App.jsx';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+);
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
+if (undefined /* [snowpack] import.meta.hot */) {
+    undefined /* [snowpack] import.meta.hot */.accept();
+}
