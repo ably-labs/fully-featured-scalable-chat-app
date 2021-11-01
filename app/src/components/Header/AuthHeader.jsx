f9448d6135f8a0b02885131058a1b75a1302a281
Srushtika Neelakantam <n.srushtika@gmail.com>
2021-11-01 16:38:34 +0000

    Restore logo.svg

--- app/src/components/Header/AuthHeader.jsx
+++ app/src/components/Header/AuthHeader.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./header.css";
-// import logo from "../../logo";
+import logo from "../../logo.svg";
 
 const Header = () => {
   return (
     <header className="authed">
-      {/* <img src={logo} alt="logo" /> */}
+      <img src={logo} alt="logo" />
       <h1>Fully Featured Scalable Chat</h1>
     </header>
   );
