ee86c6f4be8d7b3e0d0429d95b79cf68b84eccf9
thisisjofrank <jofranchetti@gmail.com>
2021-11-08 20:52:42 +0000

    Add implementations to other generic repo methods

--- api/common/dataaccess/CosmosDbMetadataRepository.ts
+++ api/common/dataaccess/CosmosDbMetadataRepository.ts
@@ -2,55 +2,56 @@ import { CosmosClient, Container } from "@azure/cosmos";
 import { Entity, IMetadataRepository } from "./IMetadataRepository";
 
 export class CosmosDbMetadataRepository implements IMetadataRepository {
+  private _client: CosmosClient;
+  private _databaseId: string;
 
-    private _client: CosmosClient;
-    private _databaseId: string;
+  constructor() {
+    const endpoint = process.env["COSMOS_ENDPOINT"];
+    const key = process.env["COSMOS_KEY"];
+    this._databaseId = process.env["COSMOS_DATABASE_ID"];
+    this._client = new CosmosClient({ endpoint, key });
+  }
 
-    constructor() {
-        const endpoint = process.env['COSMOS_ENDPOINT'];
-        const key = process.env['COSMOS_KEY'];
-        this._databaseId = process.env['COSMOS_DATABASE_ID'];
-        this._client = new CosmosClient({ endpoint, key });
+  public async getById<TEntityType extends Entity>(typeName: string, id: string): Promise<TEntityType> {
+    const container = await this.getContainer(typeName);
+    const allMatchingItems = await container.items.query(`SELECT * FROM c WHERE c.id = '${id}'`).fetchAll();
+    return allMatchingItems[0];
+  }
+
+  public async getByProperty<TEntityType extends Entity>(typeName: string, propertyName: string, value: any): Promise<TEntityType[]> {
+    const container = await this.getContainer(typeName);
+    const results = await container.items.query(`SELECT * FROM c WHERE c.${propertyName} = '${value}'`).fetchAll();
+    return results.resources as TEntityType[];
+  }
+
+  public async saveOrUpdate<TEntityType extends Entity>(entity: TEntityType): Promise<void> {
+    const container = await this.getContainer(entity.type);
+    const result = await container.items.upsert(entity);
+
+    if (result.statusCode !== 201) {
+      throw new Error(`Error saving or updating entity ${entity.id}`);
     }
+  }
 
-    public async getById<TEntityType extends Entity>(typeName: string, id: string): Promise<TEntityType> {
-        const container = await this.getContainer(typeName);
-        const allMatchingItems = await container.items.query(`SELECT * FROM c WHERE c.id = '${id}'`).fetchAll();
-        return allMatchingItems[0];
-    }
+  public async exists(typeName: string, id: string): Promise<boolean> {
+    const container = await this.getContainer(typeName);
+    const countResult = await container.items.query(`SELECT count(1) FROM c WHERE c.id = '${id}'`).fetchAll();
+    return countResult[0] > 0;
+  }
 
-    public async getByProperty<TEntityType extends Entity>(typeName: string, propertyName: string, value: any): Promise<TEntityType[]> {
-        const container = await this.getContainer(typeName);
-        const results = await container.items.query(`SELECT * FROM c WHERE c.${propertyName} = '${value}'`).fetchAll();
-        return results.resources as TEntityType[];
-    }
+  public async getAll<TEntityType extends Entity>(typeName: string): Promise<TEntityType[]> {
+    const container = await this.getContainer(typeName);
+    const results = await container.items.readAll().fetchAll();
+    return results.resources as TEntityType[];
+  }
 
-    public async saveOrUpdate<TEntityType extends Entity>(entity: TEntityType): Promise<void> {
-        const container = await this.getContainer(entity.type);
-        const result = await container.items.upsert(entity);
+  private async getContainer(typeName: string): Promise<Container> {
+    const containerId = this.generateContainerName(typeName);
+    const database = await this._client.database(this._databaseId);
+    return await database.container(containerId);
+  }
 
-        if (result.statusCode !== 201) {
-            throw new Error(`Error saving or updating entity ${entity.id}`);
-        }
-    }
-
-    public async exists<TEntityType extends Entity>(typeName: string, id: string): Promise<boolean> {
-        const containerId = this.generateContainerName(typeName);
-        return false;
-    }
-
-    public async getAll<TEntityType extends Entity>(typeName: string): Promise<TEntityType[]> {
-        const containerId = this.generateContainerName(typeName);
-        return [];
-    }
-
-    private async getContainer(typeName: string): Promise<Container> {
-        const containerId = this.generateContainerName(typeName);
-        const database = await this._client.database(this._databaseId);
-        return await database.container(containerId);
-    }
-
-    private generateContainerName(typeName: string): string {
-        return typeName.toLowerCase() + "s"; // yolo!
-    }
+  private generateContainerName(typeName: string): string {
+    return typeName.toLowerCase() + "s"; // yolo!
+  }
 }
