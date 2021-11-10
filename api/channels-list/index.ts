5dd558649882c97cce76da01991c12a51c835a4a
tomczoink <tc13270@my.bristol.ac.uk>
2021-11-10 12:32:34 +0000

    Further format changes

--- api/channels-list/index.ts
+++ api/channels-list/index.ts
@@ -9,10 +9,7 @@ export default async function (context: Context, req: HttpRequest): Promise<void
     await authorized(context, req, (authContext: ApiRequestContext) => {
 
         const channels: ChannelListResponse = {
-            channels: [
-                { name: "global-welcome" },
-                { name: "some-other-channel" }
-            ]
+            channels: [{ name: "global-welcome" }, { name: "some-other-channel" }]
         };
 
         context.res = { status: 200, body: JSON.stringify(channels) };
--- api/common/ApiRequestContext.ts
+++ api/common/ApiRequestContext.ts
@@ -4,51 +4,52 @@ import { JwtGenerator } from "./JwtGenerator";
 import { IUser, User } from "./metadata/User";
 
 export class ApiRequestContext {
-  public isAuthenticatedUser: boolean;
-  public user: IUser;
 
-  public reason: string;
+    public isAuthenticatedUser: boolean;
+    public user: IUser;
 
-  constructor(isAuthenicatedUser: boolean = false, user: IUser = null, reason: string = null) {
-    this.isAuthenticatedUser = isAuthenicatedUser;
-    this.user = user;
-    this.reason = reason;
-  }
+    public reason: string;
 
-  public static async fromRequest(req: any, includeUser: boolean = false): Promise<ApiRequestContext> {
-    const jwtValidator = JwtGenerator.fromEnvironment();
-
-    const packedJwt = req.headers.jwt || "";
-    if (packedJwt.length === 0) {
-      return new ApiRequestContext(false, null, "Token missing from request headers");
-    }
-    const { success, token } = jwtValidator.validate(packedJwt);
-
-    if (!success) {
-      return new ApiRequestContext(false, null, "Token failed to validate");
+    constructor(isAuthenicatedUser: boolean = false, user: IUser = null, reason: string = null) {
+        this.isAuthenticatedUser = isAuthenicatedUser;
+        this.user = user;
+        this.reason = reason;
     }
 
-    if (includeUser) {
-      const userId = token.body["userId"];
+    public static async fromRequest(req: any, includeUser: boolean = false): Promise<ApiRequestContext> {
+        const jwtValidator = JwtGenerator.fromEnvironment();
 
-      const repository = new CosmosDbMetadataRepository();
-      const existing = await repository.getByProperty<User>("User", "id", userId);
-      return new ApiRequestContext(true, existing[0], "Valid");
+        const packedJwt = req.headers.jwt || "";
+        if (packedJwt.length === 0) {
+            return new ApiRequestContext(false, null, "Token missing from request headers");
+        }
+        const { success, token } = jwtValidator.validate(packedJwt);
+
+        if (!success) {
+            return new ApiRequestContext(false, null, "Token failed to validate");
+        }
+
+        if (includeUser) {
+            const userId = token.body["userId"];
+
+            const repository = new CosmosDbMetadataRepository();
+            const existing = await repository.getByProperty<User>("User", "id", userId);
+            return new ApiRequestContext(true, existing[0], "Valid");
+        }
+
+        return new ApiRequestContext(true, null, "Valid");
     }
-
-    return new ApiRequestContext(true, null, "Valid");
-  }
 }
 
 export const authorized: AzureFunction = async function (context: Context, req: HttpRequest, wrappedFunction, includeUser: boolean = true): Promise<void> {
-  const ctx = await ApiRequestContext.fromRequest(req, includeUser);
-  if (!ctx.isAuthenticatedUser) {
-      context.res = { status: 401, body: JSON.stringify({ success: false, reason: ctx.reason }) };
-      return;
-  }
+    const ctx = await ApiRequestContext.fromRequest(req, includeUser);
+    if (!ctx.isAuthenticatedUser) {
+        context.res = { status: 401, body: JSON.stringify({ success: false, reason: ctx.reason }) };
+        return;
+    }
 
-  // In case a response is not set in the callback, we indicate Not Implemented
-  context.res = { status: 501, body: JSON.stringify({ success: false, reason: "Authorized, but no response set" }) };
+    // In case a response is not set in the callback, we indicate Not Implemented
+    context.res = { status: 501, body: JSON.stringify({ success: false, reason: "Authorized, but no response set" }) };
 
-  await wrappedFunction(ctx);
+    await wrappedFunction(ctx);
 };
