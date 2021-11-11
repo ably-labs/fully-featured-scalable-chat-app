9095b3a291ca12e731ce1d7f0a7d772a865211df
Srushtika Neelakantam <n.srushtika@gmail.com>
2021-11-11 14:35:01 +0000

    update auth0 redirect uri

--- app/src/script.jsx
+++ app/src/script.jsx
@@ -7,8 +7,6 @@ import "./index.css";
 
 import App from "./App.jsx";
 
-console.log(import.meta.env.SNOWPACK_PUBLIC_AUTH0_DOMAIN, import.meta.env.SNOWPACK_PUBLIC_AUTH0_CLIENT_ID, import.meta.env.SNOWPACK_PUBLIC_AUTH0_REDIRECT_URI);
-
 ReactDOM.render(
   <React.StrictMode>
     <AppProviders>
@@ -16,7 +14,7 @@ ReactDOM.render(
         <Auth0Provider
           domain={import.meta.env.SNOWPACK_PUBLIC_AUTH0_DOMAIN}
           clientId={import.meta.env.SNOWPACK_PUBLIC_AUTH0_CLIENT_ID}
-          redirectUri={import.meta.env.SNOWPACK_PUBLIC_AUTH0_REDIRECT_URI}
+          redirectUri={window.location.origin + "/auth0-landing"}
         >
           <App />
         </Auth0Provider>
