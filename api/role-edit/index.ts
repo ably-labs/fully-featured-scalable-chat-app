75a342501cdcbc4ebb00cb91c7dfb339199c6d76
tomczoink <tc13270@my.bristol.ac.uk>
2021-12-06 14:19:08 +0000

    Add role edit endpoint

---
+++ api/role-edit/index.ts
@@ -0,0 +1,55 @@
+import "../startup";
+import { Context, HttpRequest } from "@azure/functions";
+import { authorized, ApiRequestContext } from "../common/ApiRequestContext";
+import * as Validator from "validatorjs";
+import { badRequest, badRequestFor, ok } from "../common/http/CommonResults";
+import { RoleService } from "../common/services/RoleService";
+
+export default async function (context: Context, req: HttpRequest): Promise<void> {
+  await authorized(
+    context,
+    req,
+    "admin",
+    async ({ user }: ApiRequestContext) => {
+      const data = { ...req.body } as RoleEditForm
+      const validation = new Validator(data, roleEditFormRules);
+
+      if (validation.fails()) {
+        context.res = badRequest(validation);
+        return;
+      }
+
+      const roleService = new RoleService();
+      let { exists, role } = await roleService.getRoleByName(data.name);
+    
+      if (!exists) {
+        context.res = badRequestFor({
+          name: ["Role does not exist."]
+        });
+        return;
+      }
+
+      const success = await roleService.editRole(role, data.permissions);
+      if (!success) {
+        context.res = {
+          status: 500,
+          body: JSON.stringify({ success: false, reason: "Internal error editing role" })
+        };
+        return;
+      }
+
+      context.res = ok("created");
+    },
+    true
+  );
+}
+
+export type RoleEditForm = {
+  name: string;
+  permissions: string[];
+};
+
+const roleEditFormRules = {
+  name: "required|min:1",
+  permissions: "required|min:1",
+};
