5303a2d01d6891270825f1b5a6ef52f73fcc47e0
tomczoink <tc13270@my.bristol.ac.uk>
2021-12-06 13:50:04 +0000

    Create RoleDelete endpoint

--- api/common/services/RoleService.ts
+++ api/common/services/RoleService.ts
@@ -38,6 +38,20 @@ export class RoleService {
     return role;
   }
 
+  public async deleteRole(role: Role): Promise<{ success: boolean; reason?: string }> {
+    const success = await this.deleteKey(role.apiKey);
+    if (!success) {
+      return { success: false, reason: "Failed to delete Ably API key for role" };
+    }
+
+    const roleDeleted = await this._repo.delete<Role>(role);
+    if (!roleDeleted) {
+      return { success: false, reason: "Failed to delete role" };     
+    }
+    
+    return { success: true };
+  }
+
   private async createKey(name: string): Promise<{ success: boolean; key?: string }> {
     const url = `https://control.ably.net/v1/apps/${process.env.APP_ID}/keys`;
 
@@ -65,4 +79,22 @@ export class RoleService {
 
     return { success: !!key, key };
   }
+
+private async deleteKey(keyID: string): Promise<boolean> {
+  let url = `https://control.ably.net/v1/apps/${process.env.APP_ID}/keys/${keyID}/revoke`;
+
+  const headers = { 
+    "Content-type": "application/json",
+    "accept": "application/json",
+    "Authorization": "Bearer " + process.env.CONTROL_KEY,
+    "Access-Control-Allow-Origin": "*"
+  };
+
+  const response = await fetch(url, {method: 'POST', headers });
+  if (response.status != 201) {
+    return false;
+  }
+
+  return true;
+}
 }
