cdc4a97994924f889bf4ddfb468343b961e61dd6
Marc Duiker <marcduiker@users.noreply.github.com>
2022-01-17 13:07:12 +0100

    Run auto format

--- app/src/sdk/LocalUserProfileCache.js
+++ app/src/sdk/LocalUserProfileCache.js
@@ -1,24 +1,23 @@
 export class LocalUserProfileCache {
-    
-    constructor() {
-        this.cache = {};
+  constructor() {
+    this.cache = {};
+  }
+
+  async get(userId, fetchFunction) {
+    const cacheItem = this.cache[userId];
+    if (cacheItem?.expires >= Date.now()) {
+      return cacheItem.value;
     }
 
-    async get(userId, fetchFunction) {
-        const cacheItem = this.cache[userId];
-        if (cacheItem?.expires >= Date.now()) {
-            return cacheItem.value;
-        }
+    const serverCopy = await fetchFunction();
+    const newCacheItem = {
+      value: serverCopy,
+      expires: Date.now() + 1000 * 60 * 5
+    };
 
-        const serverCopy = await fetchFunction();
-        const newCacheItem = {
-            value: serverCopy,
-            expires: Date.now() + (1000 * 60) * 5
-        };
-
-        this.cache[userId] = newCacheItem;
-        return newCacheItem.value;
-    }
+    this.cache[userId] = newCacheItem;
+    return newCacheItem.value;
+  }
 }
 
 export default LocalUserProfileCache;
