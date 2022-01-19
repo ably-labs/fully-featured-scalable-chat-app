b4b85bf974d2844aae975d25401d286bb2974c15
Marc Duiker <marcduiker@users.noreply.github.com>
2022-01-19 17:26:20 +0100

    Add gravatar to UserService

--- api/account-register/index.test.ts
+++ api/account-register/index.test.ts
@@ -10,15 +10,15 @@ describe("Registration API", () => {
   });
 
   it("when any mandatory property is missing, fails", async () => {
-    const requiredProperties = ["username", "firstName", "lastName", "password", "email"];
+    const requiredProperties = ["username", "email", "firstName", "lastName", "password"];
 
     for (const prop of requiredProperties) {
       const body = {
         username: "user",
+        email: "email",
         firstName: "first",
         lastName: "last",
-        password: "password",
-        email: "email"
+        password: "password"
       };
       delete body[prop];
 
@@ -40,10 +40,10 @@ describe("Registration API", () => {
     });
     const body = {
       username: "user",
+      email: "email",
       firstName: "first",
       lastName: "last",
-      password: "password",
-      email: "email"
+      password: "password"
     };
 
     await sut(context, { body });
@@ -56,10 +56,10 @@ describe("Registration API", () => {
   it("no existing users, returns 200 created for new user created", async () => {
     const body = {
       username: "user",
+      email: "email",
       firstName: "first",
       lastName: "last",
-      password: "password",
-      email: "email"
+      password: "password"
     };
 
     await sut(context, { body });
