9aba55d48f78b8a4ba45de8669fb2997ad4d9e0a
Marc Duiker <marcduiker@gmail.com>
2022-02-18 19:04:18 +0100

    Rename to utility-channel

--- api/presence-update/index.ts
+++ api/presence-update/index.ts
@@ -11,45 +11,40 @@ import { ChannelPresenceMessage } from "../common/metadata/ChannelPresenceMessag
 
 /// This function is triggered by all channels that start with "user-presence-".
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-  await authorized(
-    context,
-    req,
-    async () => {
-      // TODO Create Ably connection
-      const options: Types.ClientOptions = { authUrl: "/api/ably/token-request" };
-      const realtime = new Realtime.Promise(options);
-      realtime.connect();
-      const globalPresenceChannelName = "global-presence-channel";
-      const globalPresenceChannel = realtime.channels.get(globalPresenceChannelName);
+  await authorized(context, req, async () => {
+    // TODO Create Ably connection
+    const options: Types.ClientOptions = { authUrl: "/api/ably/token-request" };
+    const realtime = new Realtime.Promise(options);
+    realtime.connect();
+    const utilityChannelName = "utility-channel";
+    const utilityChannel = realtime.channels.get(utilityChannelName);
 
-      const data = { ...req.body } as UserPresenceMessage;
-      const userService = new UserService();
-      const channelService = new ChannelService();
-      const user = await userService.updatePresenceStatus(data.userId, data.presenceStatus, data.lastOnlineTimeStampUTC);
-      const { channels } = await channelService.getAllChannels();
-      channels.forEach(async (channel) => {
-        if (channel.isMember(user.id)) {
-          switch (user.presenceStatus) {
-            case PresenceStatus.Offline:
-              channel.decrementOnlineCount();
-              break;
-            case PresenceStatus.Online:
-              channel.incrementOnlineCount();
-              break;
-          }
-          await channelService.updateChannel(channel);
-          // publish aggregate message to the global-presence channel.
-          const channelPresenceMessage: ChannelPresenceMessage = {
-            channelId: channel.id,
-            membersOnlineCount: channel.onlineCount
-          };
-          await globalPresenceChannel.publish("channel-presence", channelPresenceMessage);
+    const data = { ...req.body } as UserPresenceMessage;
+    const userService = new UserService();
+    const channelService = new ChannelService();
+    const user = await userService.updatePresenceStatus(data.userId, data.presenceStatus, data.lastOnlineTimeStampUTC);
+    const { channels } = await channelService.getAllChannels();
+    channels.forEach(async (channel) => {
+      if (channel.isMember(user.id)) {
+        switch (user.presenceStatus) {
+          case PresenceStatus.Offline:
+            channel.decrementOnlineCount();
+            break;
+          case PresenceStatus.Online:
+            channel.incrementOnlineCount();
+            break;
         }
-      });
-    },
-    undefined,
-    true
-  );
+        await channelService.updateChannel(channel);
+        const channelPresenceMessage: ChannelPresenceMessage = {
+          channelId: channel.id,
+          membersOnlineCount: channel.onlineCount
+        };
+        await utilityChannel.publish("channel-presence", channelPresenceMessage);
+      }
+    });
+
+    context.res = ok("success");
+  });
 };
 
 export default httpTrigger;
