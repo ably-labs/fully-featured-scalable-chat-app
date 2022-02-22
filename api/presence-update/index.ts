f6564e820ded64fe00734fc5ead1e7b9369e0fd6
Marc Duiker <marcduiker@gmail.com>
2022-02-22 16:31:49 +0100

    Add presence-publish function

--- api/presence-update/index.ts
+++ api/presence-update/index.ts
@@ -1,24 +1,15 @@
 import "../startup";
-import { Types, Realtime } from "ably/promises";
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { UserService } from "../common/services/UserService";
 import { ChannelService } from "../common/services/ChannelService";
 import { UserPresenceMessage } from "../common/metadata/UserPresenceMessage";
 import { authorized } from "../common/ApiRequestContext";
-import { etagFor, etagMatches, notFound, notModified, ok } from "../common/http/CommonResults";
+import { ok } from "../common/http/CommonResults";
 import { PresenceStatus } from "../common/metadata/PresenceStatus";
-import { ChannelPresenceMessage } from "../common/metadata/ChannelPresenceMessage";
 
 /// This function is triggered by all channels that start with "user-presence-".
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   await authorized(context, req, async () => {
-    // TODO Create Ably connection
-    const options: Types.ClientOptions = { authUrl: "/api/ably/token-request" };
-    const realtime = new Realtime.Promise(options);
-    realtime.connect();
-    const utilityChannelName = "utility-channel";
-    const utilityChannel = realtime.channels.get(utilityChannelName);
-
     const data = { ...req.body } as UserPresenceMessage;
     const userService = new UserService();
     const channelService = new ChannelService();
@@ -35,14 +26,8 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
             break;
         }
         await channelService.updateChannel(channel);
-        const channelPresenceMessage: ChannelPresenceMessage = {
-          channelId: channel.id,
-          membersOnlineCount: channel.onlineCount
-        };
-        await utilityChannel.publish("channel-presence", channelPresenceMessage);
       }
     });
-
     context.res = ok("success");
   });
 };
